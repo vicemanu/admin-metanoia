71721511b47226c0a190d854ed7b5afedf1d65a4
Victor Mielke <vicemanu31@outlook.com>
2023-06-02 19:14:50 -0300

    Removendo conteudo inutil

--- components/Logo/index.jsx
+++
@@ -1,9 +0,0 @@
-import './logo.css'
-
-export default function Logo() {
-    return (
-        <strong className='logo'>
-            Meta<span className='logo__noia'>noia</span>
-        </strong>
-    )
-}
