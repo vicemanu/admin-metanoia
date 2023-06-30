cec03e46f74131f4796e45f5200bebe4261d4d44
Victor Mielke <vicemanu31@outlook.com>
2023-06-29 23:23:56 -0300

    edição funcionando

--- src/pages/Edit/index.jsx
+++ src/pages/Edit/index.jsx
@@ -2,14 +2,15 @@ import './edit.css'
 import { useEffect, useState } from 'react'
 import Conteudo from '../../components/Conteudo'
 import { useParams } from 'react-router-dom'
-import { doc, getDoc } from 'firebase/firestore'
+import { doc, getDoc, updateDoc } from 'firebase/firestore'
 import { db } from '../../firebase'
 
 export default function Edit() {
     
     const { slug } = useParams()
     const [artigo, setArtigo] = useState([""])
-
+    const [conteudo, setConteudo] = useState([{title:"", img: [""], citation: [""], paragraph: [""], 
+    author: [""] }])
 
     useEffect(()=> {
     async function artigo() {
@@ -32,72 +33,83 @@ export default function Edit() {
     console.log(artigo)
 
 
+   async function editArticle(e) {
+
+        e.preventDefault()
+
+        const postRef = doc( db, "artigo", slug)
+
+        await updateDoc(postRef, artigo)
+        .then((snapshot)=> {
+            window.alert("atualizado com sucesso")
+        })
+        .catch((e)=> {
+            console.log(e)
+        })
+      }
+    
+
     return(
-        // <section className='newarticle'>
-        //     <form action="" className='main--form'>
-        //             <div className='form--div form--principal'>
+        <section className='newarticle'>
+            <form className='main--form'>
+                    <div className='form--div form--principal'>
 
-        //                 {/* Parte principal do artigo */}
+                        {/* Parte principal do artigo */}
 
-        //                 <label className='form_principal--title' htmlFor="titulo">
-        //                     Titulo: <input type="text" id='titulo' 
-        //                     value={artigo.title}
-        //                     onChange={
-        //                         e => setArtigo({...artigo, title: e.target.value})
-        //                         }  />
-        //                 </label>
-        //                 <label className='form_principal--img' htmlFor="background"> Background: 
-        //                     <input type="file" name="" id="background" 
-        //                         value={artigo.img.value}
-        //                         onChange={e => {
-        //                             setArtigo({...artigo, img: e.target.files[0]})
-        //                             }
-        //                         }  
-        //                     />
-        //                 </label>
-        //                 <label className='form_principal--descricao' htmlFor="descrição">
-        //                     <span>Descrição:</span>
-        //                     <textarea name="" id="descrição" cols="30" rows="10" 
-        //                         value={artigo.description}
-        //                         onChange={
-        //                             (e) => {
-        //                                 const valor = e.target.value
-        //                                 if(valor.length <= 200) {
-        //                                     setArtigo({...artigo, description: valor})
-        //                                 }
+                        <label className='form_principal--title' htmlFor="titulo">
+                            Titulo: <input type="text" id='titulo' 
+                            onChange={
+                                e => setArtigo({...artigo, title: e.target.value})
+                                }  />
+                        </label>
+                        <label className='form_principal--img' htmlFor="background"> Background: 
+                            <input type="file" name="" id="background" 
+                                onChange={e => {
+                                    setArtigo({...artigo, img: e.target.files[0]})
+                                    }
+                                }  
+                            />
+                        </label>
+                        <label className='form_principal--descricao' htmlFor="descrição">
+                            <span>Descrição:</span>
+                            <textarea name="" id="descrição" cols="30" rows="10" 
+                                onChange={
+                                    (e) => {
+                                        const valor = e.target.value
+                                        if(valor.length <= 200) {
+                                            setArtigo({...artigo, description: valor})
+                                        }
                                         
-        //                             }
-        //                         }
-        //                     ></textarea>
-        //                 </label>
-        //                 <label className='form_principal--date' htmlFor="lancamento">
-        //                     Dia de lançamento:
-        //                     <input type="date" id='lancamento'
-        //                         value={artigo.date}
-        //                         onChange={
-        //                             e => setArtigo({...artigo, date: e.target.value})
-        //                         }
-        //                      />
-        //                 </label>
-        //             </div>
+                                    }
+                                }
+                            ></textarea>
+                        </label>
+                        <label className='form_principal--date' htmlFor="lancamento">
+                            Dia de lançamento:
+                            <input type="date" id='lancamento'
+                                onChange={
+                                    e => setArtigo({...artigo, date: e.target.value})
+                                }
+                             />
+                        </label>
+                    </div>
 
-        //             {/* Parte do conteudo do artigo */}
+                    {/* Parte do conteudo do artigo */}
 
-        //             {
-        //                 conteudo.map((e , index ) => {
-        //                     return (
-        //                         <Conteudo key={index} index={index} dados={e} conteudo={conteudo} setConteudo={setConteudo} artigo={artigo} setArtigo={setArtigo}/>
-        //                     )
-        //                 })
-        //             }
+                    {/* {
+                        artigo.conteudo.map((e , index ) => {
+                            return (
+                                <Conteudo key={index} index={index} dados={e} conteudo={e.conteudo} setConteudo={setArtigo} artigo={artigo} setArtigo={setArtigo}/>
+                            )
+                        })
+                    } */}
 
-        //             <button >Add conteduo extra</button>
-        //             <button  type='button'> Upload Imagens</button>
-        //             <button  type="submit">Enviar</button>
+                    <button >Add conteduo extra</button>
+                    <button onClick={e => editArticle(e)}>Enviar</button>
                     
-        //         </form>
-        // </section>
-        <>
-        </>
+                </form>
+        </section>
+
     )
+
 }
